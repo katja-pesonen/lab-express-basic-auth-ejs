d0bea77b257bd8e213730ab7c158772c64273242
Katja Pesonen <katjapesonen1@gmail.com>
2022-06-01 15:01:41 +0300

    Work in progress...

---
+++ routes/auth.routes.js
@@ -0,0 +1,50 @@
+// routes/auth.routes.js
+const { Router } = require('express');
+const router = new Router();
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10; 
+ 
+const User = require('../models/User.model');
+
+
+/* GET signup page */
+router.get("/signup", (req, res, next) => {
+    res.render("auth/signup.ejs");
+  });
+ 
+
+// POST route ==> to process form data
+router.post('/signup', (req, res, next) => {
+    // console.log("The form data: ", req.body);
+   
+    const { username, email, password } = req.body;
+   
+    bcryptjs
+      .genSalt(saltRounds)
+      .then(salt => bcryptjs.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          // username: username
+          username,
+          email,
+          // passwordHash => this is the key from the User model
+          //     ^
+          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+          passwordHash: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+        //console.log('Newly created user is: ', userFromDB);
+        res.redirect('/userProfile') })
+
+      .catch(error => next(error))
+  });
+
+// for user profile page: 
+  router.get('/userProfile', (req, res) => {
+      res.render('users/user-profile')
+    });
+
+ 
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
 
 module.exports = router;
